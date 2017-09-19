1ae9dd506a257006948d21849287e9a27b5fe7a8
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-09-19 11:16:01 +0900

    fix args

--- scripts/get-chainer-model.sh
+++ scripts/get-chainer-model.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-wget --quiet https://github.com/knok/chainer-pix2pix/releases/download/v0.1/models.tar.gz | tar zxvf -
+wget https://github.com/knok/chainer-pix2pix/releases/download/v0.1/models.tar.gz \
+    -O - | tar zxvf -
--- scripts/get-train-data.sh
+++ scripts/get-train-data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 wget https://github.com/knok/pix2pix-tensorflow/releases/download/v1.0/edge2cats-dataset.tar.gz \
-    | tar zxvf -
+    -O - | tar zxvf -
