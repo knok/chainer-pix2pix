104202df6f124a2fd826280a929d334b0830f4dc
NOKUBI Takatsugu <knok@daionet.gr.jp>
2017-09-20 14:30:36 +0900

    new pretrained webdnn files

--- scripts/get-webdnn-model.sh
+++ scripts/get-webdnn-model.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget --quiet https://github.com/knok/chainer-pix2pix/releases/download/v0.1/out.tar.gz \
+wget --quiet https://github.com/knok/chainer-pix2pix/releases/download/v0.2/out.tar.gz \
     | tar zxvf -
